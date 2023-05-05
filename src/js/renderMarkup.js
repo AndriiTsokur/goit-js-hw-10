7fe0ab796aefbb223fa8a01883f7432b748d2156
Andrii Tsokur <tsokur@gmail.com>
2023-05-05 09:22:33 +0300

    Makes renderMarkup.js more readable

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -11,34 +11,42 @@ function renderMarkup(data) {
 	} else if (data.length > 10) {
 		return showNotification('warning');
 	} else if (data.length === 1) {
-		refs.countryInfo.innerHTML = `<div class="country-info__head">
-		<img class="flag" src="${data[0].flags.svg}" alt="${data[0].flags.alt}">
-		<h1 class="country-info__title">${data[0].name.official}</h1>
-		</div>
-		<p class="country-info__text">
-			<span class="country-info__bold">Capital: </span>
-			${data[0].capital}
-		</p>
-		<p class="country-info__text">
-			<span class="country-info__bold">Population: </span>
-			${new Intl.NumberFormat('de-DE').format(data[0].population)}
-		</p>
-		<p class="country-info__text">
-			<span class="country-info__bold">Languages: </span>
-			${Object.values(data[0].languages).join(', ')}
-		</p>`;
+		renderCountryInfo(data);
 	} else {
-		const countriesBundle = data
-			.map(country => {
-				return `<li class="country-item">
+		renderCountriesList(data);
+	}
+}
+
+function renderCountriesList(apiData) {
+	const countriesBundle = apiData
+		.map(country => {
+			return `<li class="country-item">
 						<img class="flag" src="${country.flags.svg}" alt="${country.flags.alt}">
 							${country.name.official}
 					</li>`;
-			})
-			.join('');
+		})
+		.join('');
 
-		refs.countryList.innerHTML = countriesBundle;
-	}
+	return (refs.countryList.innerHTML = countriesBundle);
+}
+
+function renderCountryInfo(apiData) {
+	return (refs.countryInfo.innerHTML = `<div class="country-info__head">
+		<img class="flag" src="${apiData[0].flags.svg}" alt="${apiData[0].flags.alt}">
+		<h1 class="country-info__title">${apiData[0].name.official}</h1>
+		</div>
+		<p class="country-info__text">
+			<span class="country-info__bold">Capital: </span>
+			${apiData[0].capital}
+		</p>
+		<p class="country-info__text">
+			<span class="country-info__bold">Population: </span>
+			${new Intl.NumberFormat('de-DE').format(apiData[0].population)}
+		</p>
+		<p class="country-info__text">
+			<span class="country-info__bold">Languages: </span>
+			${Object.values(apiData[0].languages).join(', ')}
+		</p>`);
 }
 
 export default renderMarkup;
